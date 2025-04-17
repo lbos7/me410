eb9eefa8b3e87d3e3bcdb43e50c22457b079c280
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-17 15:01:37 -0500

    Added plot for integral control

--- week3/plotting.py
+++ week3/plotting.py
@@ -9,6 +9,10 @@ with open('week3/prop.csv', 'r') as file:
 with open('week3/der.csv', 'r') as file:
     csv_reader = csv.reader(file)
     data_list_der = list(csv_reader)
+
+with open('week3/int.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    data_list_int = list(csv_reader)
     
 data_array_prop = np.array(data_list_prop, dtype=float)
 
@@ -22,6 +26,12 @@ data_array_der = data_array_der.T
 
 time_arr_der = data_array_der[0]
 
+data_array_int = np.array(data_list_int, dtype=float)
+
+data_array_int = data_array_int.T
+
+time_arr_int = data_array_int[0]
+
 fig = plt.figure(1)
 plt.plot(time_arr_prop, data_array_prop[1:].T)
 plt.xlabel('time (s)')
@@ -34,4 +44,11 @@ plt.plot(time_arr_der, data_array_der[1:].T)
 plt.xlabel('time (s)')
 plt.title('Derivative Control')
 plt.legend(['Pitch x10', 'Pitch Speed', 'Thrust', 'Motor Front', 'Motor Back'])
+plt.show()
+
+fig = plt.figure(3)
+plt.plot(time_arr_int, data_array_int[1:].T)
+plt.xlabel('time (s)')
+plt.title('Integral Control')
+plt.legend(['Pitch x10', 'Desired Pitch x10', 'Thrust', 'Motor Front', 'Motor Back'])
 plt.show()
